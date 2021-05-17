7c485e1a8cfcc665fc996105369c188b98c5da16
Florin Radu <florin.radu@roweb.ro>
2021-05-17 15:26:37 +0300

    Add code to source control

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import { Container, Row, Col } from "react-bootstrap";
+import { Route, Switch } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/sidebar.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col lg={2} id="sidebar-wrapper" className="d-none d-sm-block">
+          <Sidebar />
+        </Col>
+        <Col lg={10} xs={12} id="page-content-wrapper">
+          <Switch>
+            <Route
+              path="/products/:id"
+              render={(props) => {
+                <ProductList {...props} />;
+              }}
+            />
+          </Switch>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
